74ef47a23e3c178bfa4c1fec36f3375facde1e73
Nyseer Couse <nyseer.couse@gmail.com>
2026-02-21 16:39:27 -0500

    fixed typo

--- components/db.tsx
+++ components/db.tsx
@@ -12,7 +12,7 @@ const access: PoolOptions = {
   maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
   queueLimit: 0,
-  enableKeepAlive: true,s
+  enableKeepAlive: true,
   keepAliveInitialDelay: 0,
 };
 
